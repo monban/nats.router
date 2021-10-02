629760473a6a6152cd3728c2a2a3c4f002db2fb0
Ian Flynn <himitsu.monban@gmail.com>
2021-10-02 01:26:00 -0400

    move TestInfrastructure to utils_test

--- route_test.go
+++ route_test.go
@@ -1,36 +1,12 @@
 package router
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/nats-io/nats.go"
 )
-
-// Make sure our testing infrastructure is valid
-func TestInfrastructure(t *testing.T) {
-	subject := "HELLO"
-	data := []byte("World!")
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	RunServer(func(nc *nats.Conn) {
-		nc.Subscribe(">", func(msg *nats.Msg) {
-			l := fmt.Sprintf("Expected: {Subject: %v, Data: %v}, Received: {Subject: %v, Data: %v}", subject, string(data), msg.Subject, string(msg.Data))
-			if subject == msg.Subject && bytes.Equal(data, msg.Data) {
-				t.Log(l)
-			} else {
-				t.Error(l)
-			}
-			wg.Done()
-		})
-		nc.Publish(subject, data)
-		wg.Wait()
-	})
-}
 
 func TestRoute(t *testing.T) {
 	ctx, done := context.WithTimeout(context.Background(), 5*time.Second)
--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,11 @@
 package router
 
 import (
+	"bytes"
+	"fmt"
+	"sync"
+	"testing"
+
 	"github.com/nats-io/nats-server/test"
 	"github.com/nats-io/nats.go"
 )
@@ -12,4 +17,25 @@ func RunServer(fn func(*nats.Conn)) {
 	fn(nc)
 	nc.Close()
 	s.Shutdown()
+}
+
+// Make sure our testing infrastructure is valid
+func TestInfrastructure(t *testing.T) {
+	subject := "HELLO"
+	data := []byte("World!")
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	RunServer(func(nc *nats.Conn) {
+		nc.Subscribe(">", func(msg *nats.Msg) {
+			l := fmt.Sprintf("Expected: {Subject: %v, Data: %v}, Received: {Subject: %v, Data: %v}", subject, string(data), msg.Subject, string(msg.Data))
+			if subject == msg.Subject && bytes.Equal(data, msg.Data) {
+				t.Log(l)
+			} else {
+				t.Error(l)
+			}
+			wg.Done()
+		})
+		nc.Publish(subject, data)
+		wg.Wait()
+	})
 }
